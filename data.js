0be5e87d71c36ba950dfa79298dc62bb966bc3ef
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2025-09-01 15:47:12 -0400

    Update data.js

--- data.js
+++ data.js
@@ -7,6 +7,15 @@
 var bleh = {};
 bleh.rewrites = 4;
 
-bleh.resources = { Coal: 0 }
-bleh.upgrades = {}
-bleh.buildings = {}
+bleh.start = function() {
+    bleh.resources = { Coal: 0 };
+    bleh.upgrades = {};
+    bleh.buildings = {};
+
+    bleh.renderedgenerators = new Set();
+
+    /*  Now for the elements  */
+    const generatorbox = document.querySelector('.generatorBox');
+    const coaldisplay = document.getElementById('coalDisplay');
+    const imagebox = document.querySelector('.imageBox');
+}
