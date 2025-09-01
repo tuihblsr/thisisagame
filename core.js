4775feccbbfb4a8856757b7a6a3d88ca33f9d0e3
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2025-09-01 15:57:50 -0400

    Update core.js

--- core.js
+++ core.js
@@ -4,6 +4,12 @@
  * The main game loop and stuff
  **********************************************/
 
+bleh.updoot = function() {
+    bleh.resourcetick();
+    bleh.updateui();
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     bleh.start();
+    setInterval(bleh.updoot, 1000 / bleh.fps);
 })
