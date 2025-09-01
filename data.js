8086bb0fa3974445947cd2924ed3fd80fe3d2a76
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2025-09-01 15:39:21 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+/***********************************************
+ * Data.js
+ *
+ * For the mildly important variables n stuff
+ **********************************************/
+
+var bleh = {};
+bleh.rewrites = 4;
+
+bleh.resources = { Coal: 0 }
+bleh.upgrades = {}
+bleh.buildings = {}
